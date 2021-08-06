9060c7934d67a7799f97f483ed1818610cfbff04
Sourav Mondal <81108325+souravmondal99@users.noreply.github.com>
2021-08-06 12:24:40 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "grip_bank";
+	$servername = "ec2-52-72-125-94.compute-1.amazonaws.com";
+	$username = "rndccekxmfdvia";
+	$password = "048eb31df24c4d54155da59d56fc12f131f515e67fb69d189de40f90fad80395";
+	$dbname = "dbmf5rds68qv0d";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
