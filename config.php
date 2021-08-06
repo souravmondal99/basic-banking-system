807b3aa7947f7bbf1b48705f5791215653c272be
Sourav Mondal <81108325+souravmondal99@users.noreply.github.com>
2021-08-06 13:56:30 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
 	$password = "048eb31df24c4d54155da59d56fc12f131f515e67fb69d189de40f90fad80395";
 	$dbname = "dbmf5rds68qv0d";
 
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$conn = pg_connect("host=$servername user=$username password=$password dbname=$dbname");
 
 	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+		die("Could not connect to the database due to the following error --> ".pg_errormessage($conn));
 	}
 
 ?>
